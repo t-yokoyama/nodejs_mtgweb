3b8f7833942f191cdba1074c58cc3b954f7853ae
Ted <ted.yokoyama@gmail.com>
2015-10-17 09:13:41 -0700

    Initial commit. About a month's worth of work, off and on.

---
+++ index.js
@@ -0,0 +1,29 @@
+var express = require('express')
+var app = express();
+var http_server = require('http').Server(app);
+var server_io = require('socket.io')(http_server);
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+
+});
+
+server_io.on('connection', function(socket){
+
+  console.log('a user connected.');
+
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    server_io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', function(){
+  	console.log('a user disconnected.');
+  });
+});
+
+http_server.listen(3000, function(){
+  console.log('listening on localhost:3000...');
+});
