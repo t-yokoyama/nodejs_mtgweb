fa9549a35a21d3334de4248f2860aa65f6a11088
Ted <ted.yokoyama@gmail.com>
2015-11-23 16:19:56 -0500

    Some misc changes, added the pg module.

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var express = require('express')
 var app = express();
 var http_server = require('http').Server(app);
 var server_io = require('socket.io')(http_server);
+var pg = require('pg');
 
 app.use(express.static('public'));
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-
+  res.sendFile(__dirname + '/game.html');
 });
 
 server_io.on('connection', function(socket){
@@ -27,3 +27,4 @@ server_io.on('connection', function(socket){
 http_server.listen(3000, function(){
   console.log('listening on localhost:3000...');
 });
+
