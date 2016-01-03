48d986cc18dcfc26cb60b599fc6f2bbb82e778e6
Ted <ted.yokoyama@gmail.com>
2016-01-02 23:49:11 -0500

    Incorporated postgres user authentication/login code from a staging area branch.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
 var app = express();
 
@@ -7,12 +10,146 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-//var cookieParser = require('cookie-parser');
-//app.use(cookieParser());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// ! BEGIN LOGIN CODE
+
+console.log('begin login configuration');
+
+var passport = require('passport');
+var expressSession = require('express-session');
+var flash = require('connect-flash');
+app.use(expressSession({secret: 'mySecretKey'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+var bcrypt = require('bcrypt-nodejs');
+
+var isValidPassword = function(passwordHash, password) {
+  return bcrypt.compareSync(password, passwordHash);
+}
+
+var createHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
+}
+
+var LocalStrategy = require('passport-local').Strategy;
+var db = require('./database/db.js');
+
+passport.use('login', new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password',
+    passReqToCallback : true
+  },
+  function(req, username, password, done) {
+    console.log('searching database for user (login)');
+
+    db.query(
+      'SELECT * FROM users u WHERE u.username = $1::text',
+      [username],
+      function(err, result) {
+        if (err) {
+          return console.error('error running query', err);
+        }
+
+        if (result.rows.length < 1) {
+          console.log('user not found with username ' + username);
+          return done(null, false,
+                      req.flash('loginMessage', 'User not found'));
+        }
+
+        if (!isValidPassword(result.rows[0].password, password)) {
+          console.log('invalid password');
+          return done(null, false,
+                      req.flash('loginMessage', 'Invalid password'));
+        }
+
+        console.log('login success');
+        var user = { id: result.rows[0].id,
+                     username: username,
+                     password: result.rows[0].password,
+                     email: result.rows[0].email }
+        return done(null, user);
+      }
+    );
+  }
+));
+
+passport.use('signup', new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password',
+    passReqToCallback : true
+  },
+  function(req, username, password, done) {
+
+    findOrCreateUser = function() {
+      console.log('searching database for user (signup)');
+
+      db.query(
+        'SELECT u.password FROM users u WHERE u.username = $1::text',
+        [username],
+        function(err, result) {
+          if (err) {
+            return console.error('error running query', err);
+          }
+
+          if (result.rows.length > 0) {
+            console.log('user already exists: ' + username);
+            return done(null, false,
+                        req.flash('signupMessage', 'User already exists'));
+          }
+
+          var passwordHash = createHash(password);
+          var email = req.body.email;
+          db.query(
+            'INSERT INTO users (username, password, email) VALUES ($1::text, $2::text, $3::text) RETURNING id',
+            [username, passwordHash, email],
+            function(err, result) {
+              if (err) {
+                return console.error('error running query', err);
+              }
+
+              console.log('user registration successful');
+              var user = { id: result.rows[0].id,
+                           username: username,
+                           password: passwordHash,
+                           email: email }
+              return done(null, user);
+            }
+          );
+
+        }
+      );
+
+    };
+
+    // delay execution of findOrCreateUser()
+    process.nextTick(findOrCreateUser);
+}));
+
+console.log('end login configuration');
+
+// ! END LOGIN CODE
+
+
 var routes = require('./routes/route');
-app.use('/', routes);
+routes(app, passport);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/route.js
+++ routes/route.js
@@ -1,24 +1,69 @@
-var express = require('express');
 var db = require('../database/db.js');
 
-var router = express.Router();
+module.exports = function(app, passport) {
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('dummy', { title: 'MTG Web' });
-});
+  /* GET home page. */
+  app.get('/', function(req, res) {
+    res.render('index');
+  });
 
-router.get('/gameplay', function(req, res, next) {
+  /* GET login page. */
+  app.get('/login', function(req, res) {
+    res.render('login', { loginMessage: req.flash('loginMessage') });
+  });
 
-  db.query(
-    'SELECT c.imageurl, dl.qty FROM cards c, decks d, decklists dl WHERE d.id = dl.deck_id AND dl.card_id = c.id AND d.id = $1::int',
-    ['1'], // deck id
-    function(err, result) {
-      if (err) {
-        return console.error('error running query', err);
+  /* GET signup page. */
+  app.get('/signup', function(req, res) {
+    res.render('signup', { signupMessage: req.flash('signupMessage') });
+  });
+
+  /* GET profile page. */
+  app.get('/profile', isLoggedIn, function(req, res) {
+    res.render('profile', { user: req.user });
+  });
+
+  /* Handle login POST */
+  app.post('/login', passport.authenticate('login', {
+    successRedirect: '/profile',
+    failureRedirect: '/login',
+    failureFlash : true
+  }));
+
+  /* Handle Registration POST */
+  app.post('/signup', passport.authenticate('signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/signup',
+    failureFlash : true
+  }));
+
+  /* Handle logout. */
+  app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
+
+  // FIXME protect this with authentication
+  app.get('/gameplay', function(req, res) {
+    db.query(
+      'SELECT c.imageurl, dl.qty FROM cards c, decks d, decklists dl WHERE d.id = dl.deck_id AND dl.card_id = c.id AND d.id = $1::int',
+      ['1'], // deck id
+      function(err, result) {
+        if (err) {
+          return console.error('error running query', err);
+        }
+        res.render('gameplay', {decklist: result.rows});
       }
-      res.render('gameplay', {decklist: result.rows});
-    });
-});
+    );
+  });
 
-module.exports = router;
+}
+
+function isLoggedIn(req, res, next) {
+
+  if (req.isAuthenticated())
+    return next();
+
+  res.redirect('/login');
+}
+
+
